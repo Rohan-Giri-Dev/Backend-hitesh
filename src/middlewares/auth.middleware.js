6549fb9bb6a216bdcb51548e39711ed865aab67d
Rohan Giri <giri.dev.rohan@gmail.com>
2026-04-29 15:55:02 +0530

    add refresh token

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -11,9 +11,9 @@ export const verifyJWT = asyncHandler(async(req, res, next) =>{
             throw new ApiError(401, "unauthorized request");
         }
     
-        const decodedToken = jwt.verify(token.process.env.ACCESS_TOKEN_SECRET);
+        const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
     
-        const user = await User.findById(decodedToken?._id).select("-passwrd -refreshToken")
+        const user = await User.findById(decodedToken?._id).select("-password -refreshToken")
     
         if(!user){
             throw new ApiError(401, "Invalid access token")
