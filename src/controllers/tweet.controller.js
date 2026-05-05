cdaee31ddd69b86faf7e329158b57dcd2a20650b
Rohan Giri <giri.dev.rohan@gmail.com>
2026-05-05 13:17:45 +0530

    add delete tweet

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -34,26 +34,84 @@ const createTweet = asyncHandler(async (req, res) => {
 const getUserTweets = asyncHandler(async (req, res) => {
   // TODO: get user tweets
 
-  const {userId} = req.params;
+  const { userId } = req.params;
 
-  if(!userId){
-    throw new ApiError(401,"Error, Unauthorized - user not found ");
+  if (!userId) {
+    throw new ApiError(401, "Error, Unauthorized - user not found ");
   }
 
-  const tweet = await Tweet.findById({owner: userId})
+  const tweet = await Tweet.findById({ owner: userId });
 
   return res
-  .status(200)
-  .json(200, tweet, "User successfully fetched");
-
+    .status(200)
+    .json(new ApiResponse(200, tweet, "User successfully fetched"));
 });
 
 const updateTweet = asyncHandler(async (req, res) => {
   //TODO: update tweet
+  const { tweetId } = req.params;
+  const { content } = req.body;
+  const trimmed = content?.trim();
+
+  if (!trimmed) {
+    throw new ApiError(400, "Error , Content is unavaliable");
+  }
+
+  const userId = req.user?._id;
+  if (!userId) {
+    throw new ApiError(400, "Error , unauthorized user");
+  }
+
+  //to find if the tweet exists or not
+  const tweet = await Tweet.findById(tweetId);
+  if (!tweet) {
+    throw new ApiError(404, "Tweet not found");
+  }
+
+  if (tweet.owner.toString() !== userId.toString()) {
+    throw new ApiError(403, "You can only edit your own tweets");
+  }
+
+  const updatedTweet = await Tweet.findByIdAndUpdate(
+    tweetId,
+    {
+      content: trimmed,
+    },
+    { new: true }
+  );
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, updatedTweet, "Successfully updated tweet"));
 });
 
 const deleteTweet = asyncHandler(async (req, res) => {
   //TODO: delete tweet
+  const { tweetId } = req.params;
+
+  if (!tweetId) {
+    throw new ApiError(400, "Not found any tweetId");
+  }
+
+  const userId = req.user?._id;
+  if (!userId) {
+    throw new ApiError(400, "Error , unauthorized user");
+  }
+
+  const tweet = await Tweet.findById(tweetId);
+  if (!tweet) {
+    throw new ApiError(400, "Error , no tweet found");
+  }
+
+  if (tweet.owner.toString() !== userId.toString()) {
+    throw new ApiError(400, "You can only delete your own tweets");
+  }
+
+  const deletedTweet = await Tweet.findByIdAndDelete(tweetId);
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, deletedTweet, "Deleted the tweet"));
 });
 
 export { createTweet, getUserTweets, updateTweet, deleteTweet };
