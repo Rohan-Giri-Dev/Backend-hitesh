f519b6e4abb87552bfb8a4006772954026d6a032
Rohan Giri <giri.dev.rohan@gmail.com>
2026-05-05 12:32:17 +0530

    add createTweet

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -1,29 +1,46 @@
-import mongoose, { isValidObjectId } from "mongoose"
-import {Tweet} from "../models/tweet.model.js"
-import {User} from "../models/user.model.js"
-import {ApiError} from "../utils/ApiError.js"
-import {ApiResponse} from "../utils/ApiResponse.js"
-import {asyncHandler} from "../utils/asyncHandler.js"
+import mongoose, { isValidObjectId } from "mongoose";
+import { Tweet } from "../models/tweet.models.js";
+import { User } from "../models/user.models.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
 
 const createTweet = asyncHandler(async (req, res) => {
-    //TODO: create tweet
-})
+  //TODO: create tweet
+
+  const { content } = req.body;
+  const trimmed = content?.trim()
+
+  if (!trimmed) {
+    throw new ApiError(400, "Error, Content is required");
+  }
+
+  const userId = req.user?._id;
+
+  if (!userId) {
+    throw new ApiError(401, "Unauthorized - user not found");
+  }
+
+  const tweet = await Tweet.create({
+    content: trimmed,
+    owner: userId,
+  });
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, tweet, "Successfully created tweet"));
+});
 
 const getUserTweets = asyncHandler(async (req, res) => {
-    // TODO: get user tweets
-})
+  // TODO: get user tweets
+});
 
 const updateTweet = asyncHandler(async (req, res) => {
-    //TODO: update tweet
-})
+  //TODO: update tweet
+});
 
 const deleteTweet = asyncHandler(async (req, res) => {
-    //TODO: delete tweet
-})
+  //TODO: delete tweet
+});
 
-export {
-    createTweet,
-    getUserTweets,
-    updateTweet,
-    deleteTweet
-}
+export { createTweet, getUserTweets, updateTweet, deleteTweet };
